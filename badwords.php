5053e7a510cbbc2427618001fc6ddf63cccc10e0
Francesco-Matteucci <francescomatteucci31@gmail.com>
2024-10-02 14:05:06 +0200

    Improve UI and exercise done

--- badwords.php
+++ badwords.php
@@ -6,12 +6,16 @@
     <title>PHP Badwords - Risultato</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
 </head>
 
-<body class="bg-light">
-    <div class="container mt-5">
-        <h1 class="text-center mb-4">PHP Badwords</h1>
-        <h3 class="text-center mb-4">Risultato</h3>
+<body>
+
+<div class="container mt-5">
+        <div class="bg-primary text-white text-center p-4 rounded shadow-sm mb-5">
+            <h1>php badwords</h1>
+            <h3>risultato</h3>
+        </div>
 
         <?php
         // Recupero i dati inviati tramite GET
@@ -21,13 +25,14 @@
         // Censuro la parola con tre asterischi
         $censored_word = str_ireplace($badword, '***', $section);
         ?>
-        <div class="mb-4 p-3 bg-white shadow-sm rounded">
+
+        <div class="card-custom mb-4 p-4">
             <h4>Paragrafo che hai scritto:</h4>
             <p><?php echo $section; ?></p>
             <p class="fw-bold">Lunghezza: <?php echo strlen($section); ?> caratteri</p>
         </div>
 
-        <div class="p-3 bg-white shadow-sm rounded">
+        <div class="card-custom p-4">
             <h4>Paragrafo con la censura della parola scelta:</h4>
             <p><?php echo $censored_word; ?></p>
             <p class="fw-bold">Lunghezza: <?php echo strlen($censored_word); ?> caratteri</p>
--- index.php
+++ index.php
@@ -6,13 +6,18 @@
     <title>PHP Badwords - Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
 </head>
 
-<body class="bg-light">
-    <div class="container mt-5">
-        <h1 class="text-center mb-4">PHP Badwords</h1>
-        <h3 class="text-center mb-4">Inserisci qui il paragrafo e la parola che vuoi censurare</h3>
-        <form action="badwords.php" method="GET" class="p-4 border rounded bg-white shadow-sm">
+<body>
+<div class="container mt-5">
+
+        <div class="bg-primary text-white text-center p-4 rounded shadow-sm mb-5">
+            <h1>php badwords</h1>
+            <h3>inserisci qui il paragrafo e la parola che vuoi censurare</h3>
+        </div>
+        
+        <form action="badwords.php" method="GET" class="p-4 border rounded bg-white shadow-sm card-custom">
             <div class="mb-3">
                 <label for="section" class="form-label">Paragrafo:</label>
                 <textarea name="section" id="section" rows="5" class="form-control" required></textarea>
